637f33c4d32399c63990a051b919f7383621af14
Deimos <deimos@deimos.fr>
2013-09-19 18:07:32 +0200

    Nagios Plugin - Check if LDAP attribute exist

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ check_ldap_request
 ==================
 
 Nagios Plugin - Check if LDAP attribute exist
+
+
+# Installation
+
+- Simply put the nagios plugin check_ldap_request.pl in your nagios-plugins directory
+- Install the perl module using cpan (install Net::LDAP)
+
+# Nagios configuration
+
+> define command{<br />
+>        command_name    check_ldap_request<br />
+>        command_line    $USER2$/check_ldap_request.pl -H $HOSTADDRESS$ -b "dc=ldap,dc=company,dc=lan" -f "uid=pmavro"<br />
+> }<br />
+> <br />
+> define service{<br />
+>          use                             generic-services<br />
+>          hostgroup_name                  ldapsrv<br />
+>          service_description             LDAP request check<br />
+>          check_command                   check_ldap_request<br />
+> }
+
+# Usage
+> Usage : ./check_ldap -H hostname -b base -f filer [-s scope] [-h]<br />
+>         -H : LDAP server hostname or IP<br />
+>         -b : Base of LDAP server (ex: "dc=ldap,dc=company,dc=lan")<br />
+>         -f : Specify a filter (ex: "uid=pmavro")<br />
+>         -s : LDAP Scope (ex: sub)<br />
+>         -h : Print this help message
+
+# Examples
+
+> $ ./check_ldap_request.pl -H prd-ldap-srv -b "dc=openldap,dc=mycompany,dc=com" -f "uid=pmavro"<br />
+> LDAP request OK - 'uid=pmavro' exists
+
+> $ ./check_ldap_request.pl -H prd-ldap-srv -b "dc=openldap,dc=mycompany,dc=com" -f "cn=foo"<br />
+> LDAP request CRITICAL - unable to find 'cn=foo'
+
---
+++ check_ldap_request.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  check_ldap_request.pl
+#
+#        USAGE:  ./check_ldap_request.pl -H hostname -b base -f filer [-s scope] [-h]
+#
+#  DESCRIPTION:  Nagios Plugin - Check if LDAP attribute exist
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  Net::LDAP Perl library
+#         BUGS:  ---
+#        NOTES:  ---
+#      LICENSE:  This Nagios module is under GPLv2 License
+#       AUTHOR:  Pierre Mavro (), pierre@mavro.fr
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  09/12/2009 15:38:41
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Net::LDAP;
+
+# Make an LDAP request
+sub make_ldap_request
+{
+	my $hostname=shift;
+	my $base=shift;
+	my $filter=shift;
+	my $scope=shift;	
+
+	my $ldap=Net::LDAP->new("ldap://$hostname") or die("Connection to $hostname could not be established : $@ - $!");
+
+	my $mesg=$ldap->bind();
+	
+	$mesg=$ldap->search(base  =>$base,
+          	            scope =>"$scope",
+              	        filter=>"($filter)");
+	$mesg->code && die $mesg->error;
+	
+	if($mesg->count()) {
+		$ldap->unbind();
+		print "LDAP request OK - \'$filter\' exists\n";
+		exit(0);
+	}
+	else
+	{
+		$ldap->unbind();
+		print "LDAP request CRITICAL - unable to find \'$filter\'\n";
+		exit(2);
+	}
+} 
+
+# Help print
+sub help
+{
+    print "Usage : check_ldap_request.pl -H hostname -b base -f filer [-s scope] [-h]\n";
+    print "\t-H : LDAP server hostname or IP\n";
+    print "\t-b : Base of LDAP server (ex: \"dc=ldap,dc=company,dc=lan\")\n";
+    print "\t-f : Specify a filter (ex: \"uid=pmavro\")\n";
+    print "\t-s : LDAP Scope (ex: sub)\n";
+    print "\t-h : Print this help message\n";
+    exit(1);
+}
+
+sub check_opts
+{
+	# Vars
+	my ($hostname,$base,$filter);
+	my $scope='sub';
+	
+	# Set options
+	GetOptions( "help|h"    => \&help,
+				"H=s"       => \$hostname,
+				"b=s"		=> \$base,
+				"f=s"		=> \$filter,
+				"s=s"		=> \$scope);
+				
+	unless (($hostname) and ($base) and ($filter))
+	{
+		&help;
+	}
+	else
+	{
+		&make_ldap_request($hostname,$base,$filter,$scope);
+	}
+}
+
+&check_opts;
